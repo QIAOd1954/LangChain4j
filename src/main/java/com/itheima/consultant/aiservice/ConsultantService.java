024f5326dd85ce2651a990683e2d661b88df61e8
1954 <qiaojiale666123@gmail.com>
2025-06-29 23:37:00 +0800

    LangChain4j_会话功能_消息注解实现

--- src/main/java/com/itheima/consultant/aiservice/ConsultantService.java
+++ src/main/java/com/itheima/consultant/aiservice/ConsultantService.java
@@ -1,5 +1,8 @@
 package com.itheima.consultant.aiservice;
 
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 import reactor.core.publisher.Flux;
@@ -9,8 +12,13 @@ import reactor.core.publisher.Flux;
         chatModel = "openAiChatModel", //指定模型
         streamingChatModel = "openAiStreamingChatModel"
 )
+//@AiService
 public interface ConsultantService {
     //用于聊天的方法
 //    public String chat(String message);
-    public Flux<String> chat(String message);
+    //@SystemMessage("你是东哥助手小月月，人美心善！")
+    @SystemMessage(fromResource = "system.txt")
+    //@UserMessage("你是东哥助手小月月，人美心善！{{it}}")
+    //@UserMessage("你是东哥助手小月月，人美心善！{{msg}}")
+    public Flux<String> chat(@V("msg") String message);
 }
