aa4c3067d2a9d6679b9fde2a5e79999ded14d70c
1954 <qiaojiale666123@gmail.com>
2025-06-30 17:39:49 +0800

    LangChain4j_会话功能_会话记忆实现 一：定义、配置 二：根据对象隔离对话 三：会话记忆持久化到redis中

--- src/main/java/com/itheima/consultant/aiservice/ConsultantService.java
+++ src/main/java/com/itheima/consultant/aiservice/ConsultantService.java
@@ -1,5 +1,6 @@
 package com.itheima.consultant.aiservice;
 
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.V;
@@ -10,7 +11,9 @@ import reactor.core.publisher.Flux;
 @AiService(
         wiringMode = AiServiceWiringMode.EXPLICIT,//手动装配
         chatModel = "openAiChatModel", //指定模型
-        streamingChatModel = "openAiStreamingChatModel"
+        streamingChatModel = "openAiStreamingChatModel",
+        //chatMemory = "chatMemory",//配置会话记忆对象
+        chatMemoryProvider = "chatMemoryProvider"//配置会话记忆提供者对象
 )
 //@AiService
 public interface ConsultantService {
@@ -20,5 +23,5 @@ public interface ConsultantService {
     @SystemMessage(fromResource = "system.txt")
     //@UserMessage("你是东哥助手小月月，人美心善！{{it}}")
     //@UserMessage("你是东哥助手小月月，人美心善！{{msg}}")
-    public Flux<String> chat(@V("msg") String message);
+    public Flux<String> chat(/*@V("msg")*/@MemoryId String memoryId ,@UserMessage String message);
 }
--- src/main/java/com/itheima/consultant/config/CommonConfig.java
+++ src/main/java/com/itheima/consultant/config/CommonConfig.java
@@ -1,9 +1,13 @@
 package com.itheima.consultant.config;
 
 import com.itheima.consultant.aiservice.ConsultantService;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.ChatMemoryProvider;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.store.memory.chat.ChatMemoryStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +16,8 @@ import org.springframework.context.annotation.Configuration;
 public class CommonConfig {
     @Autowired
     private OpenAiChatModel model;
+    @Autowired
+    private ChatMemoryStore chatMemoryStore;
 
 /*    @Bean
     public ConsultantService consultantService() {
@@ -21,4 +27,29 @@ public class CommonConfig {
 
         return consultantService;
     }*/
+
+    //构建会话记忆对象
+    @Bean
+    public ChatMemory chatMemory(){
+        MessageWindowChatMemory memory = MessageWindowChatMemory.builder()
+                .maxMessages(20)
+                .build();
+        return memory;
+    }
+
+    //构建ChatMemoryProvider对象
+    @Bean
+    public ChatMemoryProvider chatMemoryProvider (){
+        ChatMemoryProvider chatMemoryProvider = new ChatMemoryProvider() {
+            @Override
+            public ChatMemory get(Object memoryId) {
+                return MessageWindowChatMemory.builder()
+                        .id(memoryId)
+                        .maxMessages(20)
+                        .chatMemoryStore(chatMemoryStore)
+                        .build();
+            }
+        };
+        return chatMemoryProvider;
+    }
 }
