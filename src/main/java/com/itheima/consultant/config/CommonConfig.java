da3f3ec0ce451dc4612a25f35817d28a830834ef
1954 <qiaojiale666123@gmail.com>
2025-06-30 21:11:35 +0800

    LangChain4j_RAG_核心API 一：文档加载器读取文档（本地磁盘绝对路径、类路径加载、根据url路径加载） 二：文档解析器解析文档内容（解析纯文本格式、pdf格式文件、微软的office文件，例如DOC、PPT、XLS和几乎所有格式的文件） 三：向量模型（把分割后的文本片段向量化或者把用户消息向量化） 四：向量数据库操作对象EmbeddingStore（操作向量数据库的API，存储和检索都需要借助于它）

--- src/main/java/com/itheima/consultant/config/CommonConfig.java
+++ src/main/java/com/itheima/consultant/config/CommonConfig.java
@@ -2,11 +2,17 @@ package com.itheima.consultant.config;
 
 import ai.djl.nn.core.Embedding;
 import com.itheima.consultant.aiservice.ConsultantService;
+import dev.langchain4j.community.store.embedding.redis.RedisEmbeddingStore;
 import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
@@ -29,6 +35,10 @@ public class CommonConfig {
     private OpenAiChatModel model;
     @Autowired
     private ChatMemoryStore chatMemoryStore;
+    @Autowired
+    private EmbeddingModel embeddingModel;
+    @Autowired
+    private RedisEmbeddingStore redisEmbeddingStore;
 
 /*    @Bean
     public ConsultantService consultantService() {
@@ -65,28 +75,40 @@ public class CommonConfig {
     }
 
     //构建向量数据库操作对象
-    @Bean
+    //@Bean
     public EmbeddingStore embdStore(){//embeddingStore的对象，这个对象的名字不能重复，
         //加载文档进内存
-        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
-        //构建向量数据库操作对象
-        InMemoryEmbeddingStore store = new InMemoryEmbeddingStore();
+        //List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
+        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content",new ApachePdfBoxDocumentParser());
+        //List<Document> documents = FileSystemDocumentLoader.loadDocuments("H:\\BaiduNetdiskDownload\\1、黑马程序员Java项目《苍穹外卖》企业级开发实战\\资料\\day07\\consultant\\src\\main\\resources\\content");
+
+        //构建向量数据库操作对象,操作的是内存版本的向量数据库
+
+        //InMemoryEmbeddingStore store = new InMemoryEmbeddingStore();
+
+        //构建文本分割器对象
+        DocumentSplitter ds = DocumentSplitters.recursive(500,100);
         //构建一个EmbeddingStoreIngestor对象，完成文本数据切割,向量化，存储
         EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
-                .embeddingStore(store)
+                //.embeddingStore(store)
+                .embeddingStore(redisEmbeddingStore)
+                .documentSplitter(ds)
+                .embeddingModel(embeddingModel)
                 .build();
         ingestor.ingest(documents);
 
-        return store;
+        return redisEmbeddingStore;
     }
 
     //构建向量数据库检索对象
     @Bean
-    public ContentRetriever contentRetriever(@Qualifier("embdStore") EmbeddingStore store){
+    public ContentRetriever contentRetriever(/*@Qualifier("embdStore") EmbeddingStore store*/){
         return EmbeddingStoreContentRetriever .builder()
-                .embeddingStore(store)
+                //.embeddingStore(store)
+                .embeddingStore(redisEmbeddingStore)
                 .minScore(0.5)
                 .maxResults(3)
+                .embeddingModel(embeddingModel)
                 .build();
     }
 }
